26ac362282cc70687e3bb847cf19e1cfaea2a952
Sikander Mirza <sikandersunny2017@gmail.com>
2026-03-31 00:12:29 +0500

    updated

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ const {
 
 await transporter.sendMail({
   from: `"${fullName || "Website Lead"} via I Hate Moving" <${process.env.MAIL_EMAIL}>`,
-  to: "contact@ihatemoving.co.uk",
+  to: "sikandersunny2017@gmail.com",
   subject: `New Quote Request - ${fullName || "User"} (${email || "No Email"})`,
   html,
   replyTo: email || undefined,
